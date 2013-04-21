c8943026551ea403e74424ce410c3d2d80d00c40
MiniChutes <tbulens@msn.com>
2013-04-21 06:59:10 -0500

    Starting to work on BatchJobScheduleService.

--- domain/src/test/groovy/org/tbulens/abs/domain/model/BatchJobMother.groovy
+++ domain/src/test/groovy/org/tbulens/abs/domain/model/BatchJobMother.groovy
@@ -3,6 +3,10 @@ package org.tbulens.abs.domain.model
 class BatchJobMother {
 
     static BatchJob create(String jobName) {
-        new BatchJob(jobName: jobName, groupName: "Group1")
+        create(jobName: jobName, groupName: "Group1", "* 0 * * ? *")
+    }
+
+    static BatchJob create(String jobName, String groupName, String cronExpression) {
+        new BatchJob(jobName: jobName, groupName: groupName, cronExpression: new CronExpression(expression: cronExpression) )
     }
 }
